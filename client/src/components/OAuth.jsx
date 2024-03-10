84692dd26d1da62546c7f48b7f114cbb45e7a7e5
Jayandra Hamal <salonhamal278@gmail.com>
2024-03-10 12:49:09 +0530

    update OAuth google

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,22 +1,20 @@
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
-import {app} from '../firebase'
-import { signInSuccess } from '../redux/user/userSlice';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
 import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
-
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
-      const provider = new GoogleAuthProvider()
-      const auth = getAuth(app)
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
 
-      const result = await signInWithPopup(provider, auth)
+      const result = await signInWithPopup(auth, provider);
 
-      const navigate = useNavigate();
-      const dispatch = useDispatch();
-
-      const res = await fetch ('/api/auth/google', {
+      const res = await fetch('/api/auth/google', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -27,18 +25,20 @@ export default function OAuth() {
           photo: result.user.photoURL,
         }),
       });
-
       const data = await res.json();
       dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      console.log("could not sign in with google", error)
+      console.log('could not sign in with google', error);
     }
-  }
-
+  };
   return (
-    <button  onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
-      continue with google
+    <button
+      onClick={handleGoogleClick}
+      type='button'
+      className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
+    >
+      Continue with google
     </button>
-  )
+  );
 }
